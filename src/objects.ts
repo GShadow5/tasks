3e46fe6467753fce37034b146786dce481b3aa88
Nayan Sawyer <33187059+GShadow5@users.noreply.github.com>
2023-09-12 20:29:31 -0400

    Done toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -79,7 +79,13 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let out = `# ${question.name}\n${question.body}`;
+    if (question.type === "multiple_choice_question") {
+        for (const id in question.options) {
+            out = out + "\n- " + question.options[id];
+        }
+    }
+    return out;
 }
 
 /**
